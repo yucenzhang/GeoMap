7d8e086039b4a9cde9c3f8ed1213c861beb438cf
ydx <x6doooo@gmail.com>
2013-06-17 12:26:14 +0800

    新todo

---
+++ src/geomap.3d.js
@@ -0,0 +1,123 @@
+var convertor_3d = {
+  "formatPoint": function(p){
+    return [
+      (p[0] < -168.5 ? p[0] + 360 : p[0]) + 170,
+      90 - p[1]
+    ];
+  },
+  "makePoint": function(p){
+    var self = this,
+      return self.formatPoint(p);
+    /*
+      x = point[0],
+      y = point[1];
+    if(self.xmin > x) self.xmin = x;
+    if(self.xmax < x) self.xmax = x;
+    if(self.ymin > y) self.ymin = y;
+    if(self.ymax < y) self.ymax = y;
+    return [x, y];
+     */
+  },
+  "Point": function(coordinates){
+    coordinates = this.makePoint(coordinates);
+    return coordinates.join(',');
+  },
+  "LineString": function(coordinates){
+    var str = '',
+      self = this,
+      i = 0,
+      len = coordinates.length,
+      point;
+
+    for( ; i < len; i++){
+      point = self.makePoint(coordinates[i]);
+      if(i == 0){
+        str = 'M' + point.join(',');
+      }else{
+        str = str + 'L' + point.join(',');
+      }
+    }
+    return str;
+  },
+  "Polygon": function(coordinates){
+    var str = '',
+      i = 0,
+      len = coordinates.length;
+
+    for(; i < len; i++){
+      str = str + convertor.LineString(coordinates[i]) + 'z';
+    }
+    return str;
+  },
+  "MultiPoint": function(coordinates){
+    var arr = [],
+      i = 0,
+      len = coordinates.length;
+    for(; i < len; i++){
+      arr.push(convertor.Point(coordinates[i]));
+    }
+    return arr;
+  },
+  "MultiLineString": function(coordinates){
+    var str = '',
+      i = 0,
+      len = coordinates.length;
+    for(; i < len; i++){
+      str += convertor.LineString(coordinates[i]);
+    }
+    return str;
+  },
+  "MultiPolygon": function(coordinates){
+    var str = '',
+      i = 0,
+      len = coordinates.length;
+    for(; i < len; i++){
+      str += convertor.Polygon(coordinates[i]);
+    }
+    return str;
+  }
+};
+
+function json2path_3d(json){
+  var
+    shapes = json.features,
+    shapeType,
+    shapeCoordinates,
+    str,
+    geometries,
+    pathArray = [],
+    i, j,
+    len, len2,
+    val,
+    shape;
+
+  convertor.xmin = 360;
+  convertor.xmax = 0;
+  convertor.ymin = 180;
+  convertor.ymax = 0;
+
+  for(i = 0, len = shapes.length; i < len; i++){
+    shape = shapes[i];
+    if(shape.type == 'Feature'){
+      pushApath(shape.geometry, shape);
+    }else if(shape.type = 'GeometryCollection'){
+      geometries = shape.geometries;
+      for(j = 0, len2 = geometries.length; j < len2; j++){
+        val = geometries[j];
+        pushApath(val, val);
+      }
+    }
+  }
+  function pushApath(gm, shape){
+    shapeType = gm.type;
+    shapeCoordinates = gm.coordinates;
+    str = convertor[shapeType](shapeCoordinates);
+    pathArray.push({
+      type: shapeType,
+      path: str,
+      properties: shape.properties,
+      id: shape.id
+    });
+  }
+  return pathArray;
+}
