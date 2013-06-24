a96aa47ba6990144b47a0ec3cb8da21a0b4270f0
ydx <x6doooo@gmail.com>
2013-06-24 17:27:05 +0800

    修正低版本ie绘制不正常的问题

--- src/geomap.svg.js
+++ src/geomap.svg.js
@@ -9,10 +9,6 @@ var GeoMap = function(cfg){
         'stroke': '#999',
         'stroke-width': 0.7
       },
-      crossline:{
-        enable: false,
-        color: '#ccc'
-      },
       background:'#fff',
       sideSize: 4
     };
@@ -27,8 +23,6 @@ var GeoMap = function(cfg){
 
   self.width = defaultCfg.width || self.container.width();
   self.height = defaultCfg.height || self.container.height();
-  self.left = self.container.offset().left;
-  self.top = self.container.offset().top;
   self.canvas = new Raphael(self.container.get(0), self.width, self.height);
   self.shapes = self.canvas.set();
   self.config = defaultCfg;
@@ -41,14 +35,6 @@ GeoMap.prototype = {
   },
   load: function(json){
     this.paths = json2path(json);
-    /* 效果不理想，暂弃
-    var onePath = '';
-    paths.forEach(function(p){
-      onePath += p.path;
-    });
-
-    this.onePath = onePath;
-    */
   },
   render: function(){
     var self = this,
@@ -60,19 +46,13 @@ GeoMap.prototype = {
       offset = config.offset,
       scale = config.scale,
       background = config.background,
-      crossline = config.crossline,
       width = self.width,
       height = self.height,
-      left = self.left + 5,
-      top = self.top + 7,
       mapleft = convertor.xmin,
       maptop = convertor.ymin,
       mapwidth = convertor.xmax - convertor.xmin,
       mapheight = convertor.ymax - convertor.ymin,
-      aPath = null, linehead, linex, liney, back, i, len, currentPath;
-
-    // 暂弃
-    // if(isRenderOnePath) paths = [{path: self.onePath}];
+      aPath = null, back, i, len, currentPath;
 
     if(!scale){
       var temx = width / mapwidth,
@@ -96,13 +76,6 @@ GeoMap.prototype = {
       'fill': background, 'stroke-width': 0
     });
 
-    linehead = 'M' + (mapleft) + ',' + (maptop);
-    linex = linehead + 'H' + convertor.xmax * scale.x;
-    liney = linehead + 'V' + convertor.ymax * scale.y;
-
-    self.crosslineX = canvas.path(linex).attr({'stroke': crossline.color, 'stroke-width': '1px'}).hide();
-    self.crosslineY = canvas.path(liney).attr({'stroke': crossline.color, 'stroke-width': '1px'}).hide();
-
     for(i = 0, len = paths.length; i < len; i++){
       currentPath = paths[i];
       if(currentPath.type == 'point' || currentPath.type == 'MultiPoint'){
@@ -113,49 +86,18 @@ GeoMap.prototype = {
       shapes.push(aPath);
     }
 
-    if(Raphael.svg){
-      canvas.setViewBox(offset.x, offset.y, width, height, false);
-      shapes.attr(style).scale(scale.x, scale.y, mapleft, maptop);
-    }else{
-      shapes.attr(style).translate(offset.x - 450, offset.y - 50).scale(scale.x, scale.y, mapleft, maptop);
-    }
+    //if(Raphael.svg){
 
-    //TODO: crossline的位置计算，由于要做一点偏移，所以偏移量要考虑到缩放问题，类似getGeoPosition方法，计算出图上偏移的实际值
-    if(crossline.enable === true){
-      shapes.mouseover(function(){
-        showCrossLine();
-      }).mousemove(function(e){
-          moveCrossLine(e);
-        }).mouseout(function(){
-          hideCrossLine();
-        });
-      back.mouseover(function(){
-        showCrossLine();
-      }).mousemove(function(e){
-          moveCrossLine(e);
-        }).mouseout(function(){
-          hideCrossLine();
-        });
-    }
-    function showCrossLine(){
-      self.crosslineX.toFront().show();
-      self.crosslineY.toFront().show();
-    }
-    function moveCrossLine(e){
-      var pos = getEventPos(e);
-      self.crosslineX.transform('T0,'+pos.y);
-      self.crosslineY.transform('T'+ pos.x + ',0');
-    }
-    function hideCrossLine(){
-      self.crosslineX.hide();
-      self.crosslineY.hide();
-    }
-    function getEventPos(e){
-      return {
-        x: parseInt(e.pageX - left) + 0.4,
-        y: parseInt(e.pageY - top) + 0.4
-      };
-    }
+     // canvas.setViewBox(offset.x, offset.y, width, height, false);
+     // shapes.attr(style).scale(scale.x, scale.y, mapleft, maptop);
+   //  shapes.attr(style).scale(scale.x, scale.y, 0, 0).translate(-offset.x, -offset.y);
+    //}else{
+      //shapes.attr(style).translate(offset.x - 450, offset.y - 50).scale(scale.x, scale.y, mapleft, maptop);
+    //shapes.attr(style).translate(-offset.x, -offset.y).scale(scale.x, scale.y, mapleft, maptop);
+    //}
+
+    shapes.attr(style).translate(-offset.x, -offset.y).scale(scale.x, scale.y, mapleft, maptop);
+
   },
   /*!
       将平面上的一个点的坐标，转换成实际经纬度坐标
@@ -169,20 +111,34 @@ GeoMap.prototype = {
       b = m.b,
       c = m.c,
       d = m.d,
-      e = m.e,
-      f = m.f;
+      e = m.e + this.config.offset.x,
+      f = m.f + this.config.offset.y;
 
     y = (y1 - f - x1 / a * b + e / a * b)/(d - c / a * b);
     x = (x1 - e - y * c) / a;
+
     y = 90 - y;
     x = x - 170;
     x = x > 180 ? x - 360 : x;
-    return [x, y];
+
+//    x = e / a;
+//    y = f / d;
+
+    if(Raphael.svg){
+      return [x, y];
+    }
+    return [x + 1, y - 1];
   },
   geo2pos: function(p){
     var	self = this,
-      matrixTrans = self.shapes[0].matrix;
-    p = convertor.makePoint([p.x, p.y]);
+      matrixTrans = self.shapes[0].matrix;//.clone();
+//    matrixTrans.e += this.config.offset.x;
+ //   matrixTrans.f += this.config.offset.y;
+    if(Raphael.svg){
+      p = convertor.makePoint([p.x, p.y]);
+    }else{
+      p = convertor.makePoint([p.x - 1, p.y + 1]);
+    }
     //通过matrix去计算点变换后的坐标
     p[0] = matrixTrans.x(p[0], p[1]);
     p[1] = matrixTrans.y(p[0], p[1]);
